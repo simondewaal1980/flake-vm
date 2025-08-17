71c9a17f3eb0315dd703311b48983aa031033969
Your Name <you@example.com>
2025-08-17 16:30:50 +0200

    tweede commit

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+# Edit tjhis configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
@@ -9,26 +9,77 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-#Nix options
-
-nixpkgs.config.allowUnfree = true; 
-
-nix.settings = {
+   nix.settings = {
 experimental-features = [ "nix-command" "flakes" ];
 };
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.cudaSupport = true;
+#Disable powermanagment
+systemd.sleep.extraConfig = ''
+  AllowSuspend=no
+  AllowHibernation=no
+  AllowHybridSleep=no
+  AllowSuspendThenHibernate=no
+'';
+#Fstab
+fileSystems = {
+"/".options = [ "compress=zstd" ];
+  "/home".options = [ "compress=zstd" ];
+  "/nix".options = [ "compress=zstd" "noatime" ];
+};
+#Zram swap
+zramSwap.enable = true;
+
+
+#Clean
+nix.gc = {
+       automatic = true;
+     dates = "daily";
+     options = "--delete-older-than 2d";
+   };
+#autoupdate
+system.autoUpgrade = {
+  enable = true;
+  flake = "/flake-vm"; 
+  flags = [
+    "--update-input"
+    "nixpkgs"
+    "-L" # print build logs
+  ];
+  dates = "19:00";
+  randomizedDelaySec = "45min";
+};
+#auto optimise Nix store
+nix.settings.auto-optimise-store = true;
+#aliassen
+environment.shellAliases ={
+ls = "ls -la";
+#flakeupd ="nix flake update /flake"; 
+ sysupgr = "sudo nixos-rebuild  boot ";
+ sysswitch = "sudo nixos-rebuild switch";  
+  
+sysconfig = "sudo nano /etc/nixos/configuration.nix";
+ sysclean  = "sudo nix-collect-garbage -d";
+ listgen = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
+
+};
+#CUDA cache 
+nix.settings.substituters = [ "https://cuda-maintainers.cachix.org" ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
    networking.hostName = "nixos"; # Define your hostname.
+networking.interfaces.enp4s0.wakeOnLan.enable = true;
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
    time.timeZone = "Europe/Amsterdam";
-
+#via
+  services.udev.packages = [ pkgs.via ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -36,24 +87,34 @@ experimental-features = [ "nix-command" "flakes" ];
   # Select internationalisation properties.
    i18n.defaultLocale = "nl_NL.UTF-8";
    console = {
-     font = "Lat2-Terminus16";
-     keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+     useXkbConfig = true; # use xkb.options in tty.
    };
 
   # Enable the X11 windowing system.
-   services.xserver.enable = true;
-   services.xserver.displayManager.lightdm.enable = true; 
-   services.xserver.desktopManager.cinnamon.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];  
+  hardware.graphics.enable = true;
+  hardware.nvidia-container-toolkit.enable = true;
+  hardware.nvidia.open = true;
+  hardware.nvidia.modesetting.enable = true;
+  hardware.graphics.extraPackages = with pkgs; [nvidia-vaapi-driver];
+services.xserver.enable = true;
+programs.appimage.enable = true;
+programs.appimage.binfmt = true;
+ services.flatpak.enable = true;
 
-  
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+programs.steam.enable = true;
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+   services.xserver.xkb.layout = "us";
+   services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+   services.printing.enable = true;
 
   # Enable sound.
   # services.pulseaudio.enable = true;
@@ -63,29 +124,87 @@ experimental-features = [ "nix-command" "flakes" ];
      pulse.enable = true;
    };
 
+   hardware.bluetooth.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.simon = {
      isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
+     extraGroups = [ "wheel" "docker" "podman" "libvirtd"  ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+       spotify  
+   ];
    };
-
+#cuda
+  environment.variables = {
+    CUDA_HOME = pkgs.cudatoolkit;
+  };
+#Distrobox
+virtualisation.podman = {
+  enable = true;
+ # dockerCompat = true;
+};
+virtualisation.docker = {
+enable = true;
+};
    programs.firefox.enable = true;
+
+#virtmanager
+virtualisation.libvirtd.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
-     git
-     github-desktop
-     google-chrome  
+     google-chrome
+     jre8
+     git 
+     libreoffice-fresh
+    distrobox       
+    linuxPackages.nvidia_x11
+cudaPackages.cudatoolkit
+    nvidia-container-toolkit
+    nvidia-docker
+    docker    
+    fuse
+    appimage-run
+    SDL
+    SDL2
+    sdl3
+    dotnetCorePackages.sdk_9_0_1xx-bin   
+   python312Full
+   via
+   vlc
+  virt-manager
+  fastfetch
+ edk2 
+ dotnet-runtime
+ icu
+  dotnet-aspnetcore
+  vscode-fhs
+  github-desktop 
+ ];
+
+fonts.packages = with pkgs; [
+ noto-fonts
+ noto-fonts-cjk-sans
+ noto-fonts-emoji
+ liberation_ttf
+ fira-code
+ fira-code-symbols
+ mplus-outline-fonts.githubRelease
+ dina-font
+ proggyfonts
+ corefonts
+# nerdfonts
+ vistafonts
 ];
+#Bashrc
+programs.bash.interactiveShellInit ="fastfetch" ;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -98,13 +217,18 @@ experimental-features = [ "nix-command" "flakes" ];
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+  services.openssh.enable = true;
+  programs.ssh.forwardX11 = true;
+  services.openssh.settings.X11Forwarding = true;
+
+  #system.autoUpgrade.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.enable = false; 
+ # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-   networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,17 +5,36 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e00c53f3-a470-47d0-8276-b296685c7d10";
-      fsType = "ext4";
+    { device = "/dev/disk/by-uuid/0c857ebf-3229-46f6-bf3b-bd746a74f906";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/0c857ebf-3229-46f6-bf3b-bd746a74f906";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/0c857ebf-3229-46f6-bf3b-bd746a74f906";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/12CE-A600";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
@@ -25,7 +44,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
